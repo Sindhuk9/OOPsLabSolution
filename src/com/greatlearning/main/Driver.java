ab3969ceab3b2a55d7ae49ca896aa45ea96a5bc9
sindhu <279.sindhu@gmail.com>
2021-08-30 11:33:31 +0530

    First Assesment commit

---
+++ src/com/greatlearning/main/Driver.java
@@ -0,0 +1,31 @@
+package com.greatlearning.main;
+
+import java.util.Scanner;
+
+import com.greatlearning.model.Employee;
+import com.greatlearning.service.CredentialService;
+
+public class Driver{
+
+	private final static Scanner sc= new Scanner(System.in);
+	
+	public static void main(String args[]) {
+		
+		String Firstname=sc.next();
+		String Lastname=sc.next();		
+		int choice=displayDepartment();
+		Employee e= new Employee(Firstname,Lastname,choice);			
+		CredentialService cs= new CredentialService();
+		cs.showCredentials(e);
+	}
+	
+
+	private static int displayDepartment() {
+		System.out.println("Please enter the department from the following");
+		System.out.print("1.Technical");
+		System.out.print("2.Admin");
+		System.out.print("3.Human Resource");
+		System.out.print("4.Legal");
+		return sc.nextInt();
+	}
+}
